874b58b0872e0d0b9bcae3e0282adade221d7d7d
hoangchuong <nguyenlehoangchuong1711@gmail.com>
2025-11-11 01:10:58 +0700

    a

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -11,6 +11,12 @@ export class User {
 
   @Prop({ required: true })
   password: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
 }
 
 export type UserDocument = User & Document;
